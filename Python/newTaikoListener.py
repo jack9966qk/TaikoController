20d98449c4e6673c707a1fa83e9bfc56e28442e2
jack9966qk <jack9966qk@gmail.com>
2017-05-05 21:31:19 +1000

    Added Python files

---
+++ Python/newTaikoListener.py
@@ -0,0 +1,192 @@
+# taikolistener.py
+
+import usbmux
+import SocketServer
+import select
+from optparse import OptionParser
+import sys
+import threading
+import struct
+import binascii
+import ctypes
+import time
+
+ldon = 0x21
+rdon = 0x24
+lka = 0x20
+rka = 0x25
+esc = 0x01
+spacebar = 0x39
+
+keycodes = [ldon, rdon, lka, rka, esc, spacebar]
+
+locks = {}
+for key in keycodes:
+	locks[key] =  threading.Lock()
+
+SendInput = ctypes.windll.user32.SendInput
+
+# C struct redefinitions
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+def process_command(command):
+	if command == 1:
+		print "Left Don"
+		pressAndRelease(ldon)
+	elif command == 2:
+		print "Right Don"
+		pressAndRelease(rdon)
+	elif command == 3:
+		print "Left Ka"
+		pressAndRelease(lka)
+	elif command == 4:
+		print "Right Ka"
+		pressAndRelease(rka)
+	elif command == 5:
+		print "Escape"
+		pressAndRelease(esc)
+	elif command == 6:
+		print "Spacebar"
+		pressAndRelease(spacebar)
+
+class PeerTalkThread(threading.Thread):
+	def __init__(self,*args):
+		self._psock = args[0]
+		self._running = True
+		threading.Thread.__init__(self)
+
+	def run(self):
+		framestructure = struct.Struct("! I I I I")
+		print "listening to commands"
+		while self._running:
+			try:
+				msg = self._psock.recv(16)	# frame header
+				if len(msg) > 0:
+					frame = framestructure.unpack(msg)
+					# print "frame: ", frame
+					size = frame[3]			# get size of payload?
+					msgdata = self._psock.recv(size)	# receive payload?
+					if msgdata:
+						command = binascii.hexlify(msgdata)
+						process_command(int(command))
+					else:
+						print "No data"
+			except:
+				pass
+
+	def stop(self):
+		self._running = False
+
+class KeyReleaser(threading.Thread):
+	def __init__(self, keycode, *args):
+		threading.Thread.__init__(self)
+		self.keycode = keycode
+
+	def run(self): 
+		time.sleep(0.02)
+		release(self.keycode)
+
+
+def press(keycode):
+	with locks[keycode]:
+		ReleaseKey(keycode)
+		PressKey(keycode)
+
+def release(keycode):
+	with locks[keycode]:
+		ReleaseKey(keycode)
+
+def pressAndRelease(keycode):
+	press(keycode)
+	rel = KeyReleaser(keycode)
+	print "start thread"
+	rel.start()
+
+
+print "starting..."
+mux = usbmux.USBMux()
+
+# print "Waiting for devices...",
+# while True:
+# 	try:
+# 		print "trying..."
+# 		mux.process()	# search for devices with timeout=1.0?
+# 		dev = mux.devices[0]	# get device found
+# 		print "try connect"
+# 		psock = mux.connect(dev, 2333)	# connect to device with port 2333
+# 	except usbmux.MuxError:
+# 		print "...",
+# 		time.sleep(0.5)
+# 
+# print "\nconnected to device %s" % str(dev)
+# psock.setblocking(0)	# set to non-block
+# psock.settimeout(2)		# set timeout = 2
+
+print "Waiting for devices..."
+if not mux.devices:
+    mux.process()	# search for devices with timeout=1.0?
+if not mux.devices:
+    print "No device found"
+
+dev = mux.devices[0]	# get device found
+print "connecting to device %s" % str(dev)
+psock = mux.connect(dev, 2333)	# connect to device with port 2333
+psock.setblocking(0)	# set to non-block
+psock.settimeout(2)		# set timeout = 2
+
+
+ptthread = PeerTalkThread(psock)
+ptthread.start()
+
+cmd = raw_input("press enter to exit")
+print "exiting..."
+
+# end connection and quit
+ptthread.stop()
+ptthread.join()
+psock.close()
---
+++ Python/testPress.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0
+
+while True:
+    time.sleep(0.01)
+    pyautogui.press("l")
